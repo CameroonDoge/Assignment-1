44109f083451565dc53ae7b25d87cbc90d7acc5c
Cameran Dodge <cameran.dodge@gmail.com>
2018-09-04 15:04:21 -0400

    Created endpoint to serve the listings.json file.

--- server.js
+++ server.js
@@ -7,8 +7,20 @@ var http = require('http'),
 var listingData, server;
 
 var requestHandler = function(request, response) {
-  var parsedUrl = url.parse(request.url);
-
+	console.log('New connection');
+	var parsedUrl = url.parse(request.url);
+  
+  if(parsedUrl.pathname == '/listings'){
+	  response.writeHead(200, {'Content-Type': 'application/json'});
+	  response.write(listingData);
+	  response.end();
+	  }
+  else{
+	  response.writeHead(404, {'Content-Type': 'text/plain'});
+	  response.write('Bad gateway error');
+	  response.end();
+  }
+  
   /*
     Your request handler should send listingData in the JSON format if a GET request 
     is sent to the '/listings' path. Otherwise, it should send a 404 error. 
@@ -19,6 +31,14 @@ var requestHandler = function(request, response) {
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
+  
+  // Initialize listingData variable
+  listingData = data;
+  
+  // Start server
+  server = http.createServer(requestHandler).listen(port);
+  console.log('Server started');
+  
   /*
     This callback function should save the data in the listingData variable, 
     then start the server. 
